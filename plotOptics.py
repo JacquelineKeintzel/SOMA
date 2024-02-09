84a8102aacf21190f05b85f1b143b89041f6f7ce
Jacqueline Keintzel <jacqueline.keintzel@cern.ch>
2024-02-09 12:01:16 +0900

    update plotting

--- plotOptics.py
+++ plotOptics.py
@@ -6,11 +6,6 @@ import os
 import sys
 
 
-def parabola_fit(dpp, disp, disp2, d):
-    orbit = disp*dpp + disp2*dpp**2 + d 
-    return orbit
-
-
 def read_phase(ff):
     with open(ff) as f: lines=f.readlines()
     f.close()
@@ -34,14 +29,11 @@ def read_beta_amp(ff):
 def read_beta_ph(ff):
     with open(ff) as f: lines=f.readlines()
     f.close()
-    rms = float((lines[12].split()[3])[1:])
-    # print(rms)
-    # quit()
     S = [float(lines[15+i].split()[1]) for i in range(len(lines[15:]))]
     deltabet = np.array([float(lines[15+i].split()[10]) for i in range(len(lines[15:]))])*100
     errdeltabet = np.array([float(lines[15+i].split()[11]) for i in range(len(lines[15:]))])*100
 
-    return S, deltabet, errdeltabet, rms
+    return S, deltabet, errdeltabet
 
 
 def read_beta_cod(ff):
@@ -109,21 +101,18 @@ def read_model(ff):
 
 
 
-def plot_delta(direc, name, axis, S, delta, errdelta, pngpdf, num, rms=0):
-    fix = 10
-    fiy = 3.5
+def plot_delta(direc, name, axis, S, delta, errdelta, pngpdf, num):
+    fix = 12
+    fiy = 5
     num = 1
-    size=20
+    size=18
     caps=6
     mark=7.5
 
     plt.figure(num, figsize=(fix, fiy))
-    # plt.errorbar(np.array(S)*1e-3, delta, yerr=errdelta, fmt = 'o', ms=mark, mec= '#d62728', mfc = 'None', capsize=caps, c = '#d62728', label = str(rms)+'%')
-    if rms==0:
-        plt.errorbar(np.array(S)*1e-3, delta, yerr=errdelta, fmt = 'o', ms=mark, mec= 'C0', mfc = 'None', capsize=caps, c = 'C0')
-    else:
-        plt.errorbar(np.array(S)*1e-3, delta, yerr=errdelta, fmt = 'o', ms=mark, mec= 'C0', mfc = 'None', capsize=caps, c = 'C0', label = str(rms)+' %')
-    
+    plt.errorbar(np.array(S)*1e-3, delta, yerr=errdelta, fmt = 'o', ms=mark, mec= 'C0', mfc = 'None', capsize=caps, c = 'C0', label = 'Measurement')
+    plt.tick_params('both', labelsize=size)
+    plt.xlabel('S [km]', fontsize=size)
     if name == 'COD_beta':
         #plt.ylim(-30,30)
         plt.ylabel(r'$\Delta \beta_{x}^{cod}$/$\beta_{x}^{mdl}$ [%]', fontsize=size) if axis =='x' else plt.ylabel(r'$\Delta \beta_{y}^{cod}$/$\beta_{y}^{mdl}$ [%]', fontsize=size) 
@@ -136,14 +125,9 @@ def plot_delta(direc, name, axis, S, delta, errdelta, pngpdf, num, rms=0):
         plt.ylabel(r'$\Delta \beta_{x}^{ph}$/$\beta_{x}^{mdl}$ [%]', fontsize=size) if axis =='x' else plt.ylabel(r'$\Delta \beta_{y}^{ph}$/$\beta_{y}^{mdl}$ [%]', fontsize=size) 
     if name == 'disp':
         plt.ylabel(r'$\Delta \eta_{x}$/$\eta_{x}^{mdl}$ [%]', fontsize=size) if axis =='x' else plt.ylabel(r'$\Delta \eta_{y}$/$\eta_{y}^{mdl}$ [%]', fontsize=size) 
-    plt.tick_params('both', labelsize=size)
-    plt.xlabel('S [km]', fontsize=size)
-    plt.legend(fontsize=size, loc=9)
     plt.xlim(0, 3.016)
     plt.tight_layout()
     plt.savefig(os.path.join(direc, name+'_'+axis+'_BEAT.'+pngpdf), bbox_inches='tight')
-    if name == 'beta_ph':
-        plt.show()
     plt.close(num)
     num=num+1
 
@@ -151,31 +135,28 @@ def plot_delta(direc, name, axis, S, delta, errdelta, pngpdf, num, rms=0):
 
 
 def plot_abs(direc, name, axis, S, Smdl, val, errval, valmdl, pngpdf, num):
-    fix = 12
-    fiy = 5
+    fix = 10
+    fiy = 4
     num = 1
-    size=26
+    size=22
     caps=6
     mark=7.5
 
     plt.figure(num, figsize=(fix, fiy))
-    plt.plot(np.array(Smdl)*1e-3, valmdl, c = 'C1', label='Mdl')
-    # plt.errorbar(np.array(S)*1e-3, val, yerr=errval, fmt = 'o', ms=mark, mec= 'C0', mfc = 'None', capsize=caps, c = 'C0', label = 'Measurement')
-    plt.errorbar(np.array(S)*1e-3, val, yerr=errval, fmt = 'o', ms=mark, mec= 'C0', mfc = 'None', capsize=caps, c = 'C0', label = 'Meas')
+    plt.plot(np.array(Smdl)*1e-3, valmdl, c = '#1f77b4', label='Model')
+    plt.errorbar(np.array(S)*1e-3, val, yerr=errval, fmt = 'o', ms=mark, mec= 'C0', mfc = 'None', capsize=caps, c = 'C0', label = 'Measurement')
     plt.tick_params('both', labelsize=size)
     plt.xlabel('S [km]', fontsize=size)
     if name == 'disp':
         plt.ylabel(r'$\eta_{x}$ [m]', fontsize=size) if axis =='x' else plt.ylabel(r'$\eta_{y}$ [m]', fontsize=size) 
     if name == 'disp2':
-        # plt.ylim(-50,50)
         plt.ylabel(r'$\eta_{x}^{(2)}$ [m]', fontsize=size) if axis =='x' else plt.ylabel(r'$\eta_{y}^{(2)}$ [m]', fontsize=size)
     if name == 'norm_disp':
         plt.ylabel(r'$\eta_{n,x}$ [$\sqrt{\mathregular{m}}$]', fontsize=size) if axis =='x' else plt.ylabel(r'$\eta_{n,y}$ [$\sqrt{\mathregular{m}}$]', fontsize=size) 
     if name == 'COD_disp':
         plt.ylabel(r'$\eta_{x}^{cod}$ [m]', fontsize=size) if axis =='x' else plt.ylabel(r'$\eta_{y}^{cod}$ [m]', fontsize=size) 
     plt.xlim(0, 3.016)
-    # plt.legend(loc = 9, ncol = 2, fontsize=size, bbox_to_anchor=(0.5, 1.42), fancybox=True,  numpoints=1, scatterpoints = 1)
-    plt.legend(fontsize=size, ncol=2)
+    plt.legend(loc = 9, ncol = 2, fontsize=size, bbox_to_anchor=(0.5, 1.42), fancybox=True,  numpoints=1, scatterpoints = 1)
     plt.tight_layout()
     plt.savefig(os.path.join(direc, name+'_'+axis+'_ABS.'+pngpdf), bbox_inches='tight')
     plt.close(num)
@@ -233,8 +214,8 @@ def main():
         num = plot_delta(os.path.join(options.dir, 'average'), 'beta_amp', options.axis, S, deltabet, errdeltabet, options.pngpdf, num)
     
     if 'beta_phase_'+options.axis+'.tfs' in files:
-        S, deltabet, errdeltabet, rms = read_beta_ph(os.path.join(options.dir, 'average/beta_phase_'+options.axis+'.tfs'))
-        num = plot_delta(os.path.join(options.dir, 'average'), 'beta_ph', options.axis, S, deltabet, errdeltabet, options.pngpdf, num, rms)
+        S, deltabet, errdeltabet = read_beta_ph(os.path.join(options.dir, 'average/beta_phase_'+options.axis+'.tfs'))
+        num = plot_delta(os.path.join(options.dir, 'average'), 'beta_ph', options.axis, S, deltabet, errdeltabet, options.pngpdf, num)
     
     if 'normalised_dispersion_'+options.axis+'.tfs' in files:
         S, ndx, errndx, dx, errdx = read_norm_disp(os.path.join(options.dir, 'average/normalised_dispersion_'+options.axis+'.tfs'))
@@ -249,36 +230,27 @@ def main():
 
         if len(d2x) > 1:
             tw_files = [ff for ff in os.listdir(options.model) if 'twiss_dp0' in ff ]
+            dpp = np.arange(-1e-3, 1.1e-3, 1e-4)
             orbit=[]
-            dpp=[]
             for ff in tw_files:
-                dpp.append(float(ff.split('_')[2][:-4]))
                 valmdl = read_model(os.path.join(options.model, ff))
                 orbit.append(valmdl[5]) if options.axis == 'x' else orbit.append(valmdl[6])
             orbit=np.transpose(orbit)
             
             d2mdl = []
             dmdl = []
-            for i in orbit:
-                from scipy.optimize import curve_fit
-                popt, pcov = curve_fit(parabola_fit, dpp, i)
-                d2mdl.append(popt[1])
-
-                # quit()
-
-                # diff1 = np.zeros(dpp.shape,np.float)
-                # diff2 = np.zeros(dpp.shape,np.float)
+            for i in range(len(orbit)):
                 
-                # diff1[0:-1] = np.diff(orbit[i])/np.diff(dpp)
-                # diff1[-1] = (orbit[i][-1] - orbit[i][-2])/(dpp[-1] - dpp[-2])
-                # diff2[0:-1] = np.diff(diff1)/np.diff(dpp)
-                # diff2[-1] = (diff1[-1] - diff1[-2])/(dpp[-1] - dpp[-2])
+                diff1 = np.zeros(dpp.shape,np.float)
+                diff2 = np.zeros(dpp.shape,np.float)
+                
+                diff1[0:-1] = np.diff(orbit[i])/np.diff(dpp)
+                diff1[-1] = (orbit[i][-1] - orbit[i][-2])/(dpp[-1] - dpp[-2])
+                diff2[0:-1] = np.diff(diff1)/np.diff(dpp)
+                diff2[-1] = (diff1[-1] - diff1[-2])/(dpp[-1] - dpp[-2])
 
-                # dmdl.append(diff1[10])
-                # d2mdl.append(diff2[10]*1e-3)
-
-            # print(d2mdl)
-            
+                dmdl.append(diff1[10])
+                d2mdl.append(diff2[10])
 
             num = plot_abs(os.path.join(options.dir, 'average'), 'disp2', options.axis, S, Smdl, d2x, errd2x, d2mdl, options.pngpdf, num)
             
